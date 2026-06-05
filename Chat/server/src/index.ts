8d86afd735f6951d0a8cd9b0022cfe8700a1054d
Odiel Leon <odiel.leon@gmail.com>
2026-06-05 06:43:55 -0400

    - upgraded dependencies

--- Chat/server/src/index.ts
+++ Chat/server/src/index.ts
@@ -1,5 +1,5 @@
 import { Server } from '@o-json-rpc/o-json-rpc-ts';
-import type { Api, ProcedureName } from '@o-json-rpc/o-json-rpc-ts';
+import type { ProcedureName } from '@o-json-rpc/o-json-rpc-ts';
 import { generateClients } from '@o-json-rpc/o-json-rpc-clients';
 import {
     AliasResource,
@@ -24,21 +24,19 @@ const server = new Server({
     port,
 });
 
-const v1 = 'v1' as Api;
-
 server
-    .registerResource(v1, AliasResource.name, AliasResource.schema)
-    .registerResource(v1, UsersListResource.name, UsersListResource.schema)
-    .registerResource(v1, MessagesResource.name, MessagesResource.schema)
-    .registerResource(v1, InputMessageResource.name, InputMessageResource.schema)
-    .registerResource(v1, MessageResource.name, MessageResource.schema)
-    .registerResource(v1, UserResource.name, UserResource.schema)
-    .registerProcedure(v1, 'createAlias' as ProcedureName, createAlias, { input: AliasResource.name })
-    .registerProcedure(v1, 'getUsers' as ProcedureName, getUsers, { output: UsersListResource.name })
-    .registerProcedure(v1, 'getMessages' as ProcedureName, getMessagesFn, { output: MessagesResource.name })
-    .registerProcedure(v1, 'sendMessage' as ProcedureName, sendMessageFn, { input: InputMessageResource.name })
-    .registerSubscription(v1, MessageResource.name)
-    .registerSubscription(v1, UserResource.name, {
+    .registerResource('v1', AliasResource.name, AliasResource.schema)
+    .registerResource('v1', UsersListResource.name, UsersListResource.schema)
+    .registerResource('v1', MessagesResource.name, MessagesResource.schema)
+    .registerResource('v1', InputMessageResource.name, InputMessageResource.schema)
+    .registerResource('v1', MessageResource.name, MessageResource.schema)
+    .registerResource('v1', UserResource.name, UserResource.schema)
+    .registerProcedure('v1', 'createAlias' as ProcedureName, createAlias, { input: AliasResource.name })
+    .registerProcedure('v1', 'getUsers' as ProcedureName, getUsers, { output: UsersListResource.name })
+    .registerProcedure('v1', 'getMessages' as ProcedureName, getMessagesFn, { output: MessagesResource.name })
+    .registerProcedure('v1', 'sendMessage' as ProcedureName, sendMessageFn, { input: InputMessageResource.name })
+    .registerSubscription('v1', MessageResource.name)
+    .registerSubscription('v1', UserResource.name, {
         onClientConnect: onUserSubscriptionConnect,
         onClientDisconnect: onUserSubscriptionDisconnect,
     })
--- ToDo/server/src/index.ts
+++ ToDo/server/src/index.ts
@@ -1,5 +1,5 @@
 import { Server } from '@o-json-rpc/o-json-rpc-ts';
-import type { Api, ProcedureName } from '@o-json-rpc/o-json-rpc-ts';
+import type { ProcedureName } from '@o-json-rpc/o-json-rpc-ts';
 import { generateClients } from '@o-json-rpc/o-json-rpc-clients';
 
 import { addTaskFn, deleteTaskFn, getTasksFn, TaskCollectionResource, TaskIdResource, TaskResource, TaskToggleResource, toggleTaskFn } from './procedures.ts';
@@ -12,17 +12,15 @@ const server = new Server({
     port,
 });
 
-const v1 = 'v1' as Api;
-
 server
-    .registerResource(v1, TaskResource.name, TaskResource.schema)
-    .registerResource(v1, TaskToggleResource.name, TaskToggleResource.schema)
-    .registerResource(v1, TaskIdResource.name, TaskIdResource.schema)
-    .registerResource(v1, TaskCollectionResource.name, TaskCollectionResource.schema)
-    .registerProcedure(v1, 'addTask' as ProcedureName, addTaskFn, { input: TaskResource.name })
-    .registerProcedure(v1, 'toggleTask' as ProcedureName, toggleTaskFn, { input: TaskToggleResource.name })
-    .registerProcedure(v1, 'deleteTask' as ProcedureName, deleteTaskFn, { input: TaskIdResource.name })
-    .registerProcedure(v1, 'getTasks' as ProcedureName, getTasksFn, { output: TaskCollectionResource.name })
+    .registerResource('v1', TaskResource.name, TaskResource.schema)
+    .registerResource('v1', TaskToggleResource.name, TaskToggleResource.schema)
+    .registerResource('v1', TaskIdResource.name, TaskIdResource.schema)
+    .registerResource('v1', TaskCollectionResource.name, TaskCollectionResource.schema)
+    .registerProcedure('v1', 'addTask' as ProcedureName, addTaskFn, { input: TaskResource.name })
+    .registerProcedure('v1', 'toggleTask' as ProcedureName, toggleTaskFn, { input: TaskToggleResource.name })
+    .registerProcedure('v1', 'deleteTask' as ProcedureName, deleteTaskFn, { input: TaskIdResource.name })
+    .registerProcedure('v1', 'getTasks' as ProcedureName, getTasksFn, { output: TaskCollectionResource.name })
     .start();
 
 const scriptDir = import.meta.dirname;
